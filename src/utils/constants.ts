ade92e06f4ba914759cecea9ba1a1a1571d815aa
MurodilNorboev <179554495+MurodilNorboev@users.noreply.github.com>
2025-02-21 02:05:46 +0900

    tuzatish

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,5 @@
 // utils/constants.ts
 export const baseAPI = process.env.REACT_APP_API_URL;
 
-if (!baseAPI) {
-    throw new Error("API URL is not defined");
-} else {
-    console.log("API URL is successfully defined");
-}
-
 // export const baseAPI = 'http://109.71.242.131:8082'
 // export const baseAPI = 'http://localhost:8081'
